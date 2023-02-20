c329d9fee5eda329d71136981e5b11f5c873f53e
Superb23 <1277161299@qq.com>
2023-02-20 16:28:56 +0800

    login-main

--- src/views/login/components/login-container.vue
+++ src/views/login/components/login-container.vue
@@ -1,28 +1,99 @@
 <template>
   <div class="login-container">
     <h1>superb课程管理系统</h1>
-    <el-form class="login-form">
-      <el-form-item>
-        <el-input placeholder="请输入用户名">
+    <!-- @keyup.enter="onLogin" 监听键盘enter点击 -->
+    <el-form class="login-form" 
+            :model="userInfo"
+            :rules="rules"
+            @keyup.enter="onLogin"
+            ref="refForm"
+    >
+    <!-- prop="userName"  对于校验规则rules中的要求 -->
+      <el-form-item prop="userName">
+        <el-input placeholder="请输入用户名"
+                  v-model="userInfo.userName"
+        >
           <template #prepend>
             <el-icon><Avatar /></el-icon>
           </template>
         </el-input>
       </el-form-item>
-      <el-form-item>
-        <el-input placeholder="请输入密码" show-password>
+      <el-form-item prop="password">
+        <el-input placeholder="请输入密码" 
+                  v-model="userInfo.password"
+                  show-password
+        >
           <template #prepend>
             <el-icon><Key /></el-icon>
           </template>
         </el-input>
       </el-form-item>
-      <el-button type="primary" class="login-submit">登录</el-button>
-      <div class="login-register">注册</div>
+      <el-button type="primary"
+                 class="login-submit"
+                 @click="onLogin"
+      >
+        登录
+      </el-button>
+      <div class="login-register">
+        <span class="text"
+              @click="goRegister">
+          注册
+        </span>
+      </div>
     </el-form>
-  </div>
+  </div>  
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import router from '@/router';
+
+// 获取元素-html实例
+const refForm = ref()
+// 表单数据声明
+const userInfo = ref({
+  userName: "",
+  password: ""
+})
+// 表单数据校验规则
+const rules = {
+  userName: [{
+    required: "true", // 必填
+    trigger: "blur",  // 失去焦点即进行验证
+    message: "用户名不能为空"
+  }],
+  password: [{
+    required: "true",
+    trigger: "blur",
+    message: "密码不能为空"
+  }]
+}
+// 登录的方法
+const onLogin = () => {
+  // 任意表单项校验后触发
+  refForm.value.validate((value) => {
+    if (value) {
+      // 校验成功
+      getLoginData()
+    } else {
+      // 校验失败
+    }
+  })
+}
+// 登录接口
+const getLoginData = () => {
+  localStorage.setItem("token", 1)
+  // 消息提示
+  ElMessage({
+    message: "登录成功",
+    type: 'success',
+  })
+  router.push("/home")
+}
+// 跳转注册
+const goRegister = () => {
+  router.push("/register")
+}
 
 </script>
 
@@ -57,6 +128,9 @@
       width: 100%;
       font-size: 12px;
       margin: 0 auto;
+      .text {
+        cursor:pointer; 
+      }
     }
     :deep(.el-input-group__prepend) {
       padding: 0 10px;
